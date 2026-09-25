a173ac8f1a1528012665899d3a1e6c99cef3a0f6
b3m2a1 <b3m2a1@gmail.com>
2026-09-25 16:00:34 -0500

    Add sella to container

---
+++ prelaunch.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+module purge
+export SINGULARITYENV_PATH="/opt/conda/envs/aimnet2/bin:/opt/conda/condabin:/opt/conda/bin:/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
